cb81d62a038ced444f4c4cf9d7ed6a1bd2f876f1
kawanaza <kawanbabani@gmail.com>
2021-12-14 11:46:12 +0100

    Done

--- index.js
+++ index.js
@@ -6,66 +6,69 @@ Eigenschaften:
 3. Himmelsrichtung
 */
 let eddy = {
-    posX: 0,
-    posY: 0,
-    // Orientierung - Himmelsrichtung (Norden, Osten, Süden, Westen)
-    heading: 'EAST'
+  posX: 0,
+  posY: 0,
+  // Orientierung - Himmelsrichtung (Norden, Osten, Süden, Westen)
+  heading: "EAST",
 };
 
 /*
-Funktionen des Roboters
-1. step() -> bewegt sich einen Schritt in seine Himmelsrichtung
-2. rotate() -> rotiert sich um seine eigene Achse im Uhrzeigersinn
-*/
+    Funktionen des Roboters
+    1. step() -> bewegt sich einen Schritt in seine Himmelsrichtung
+    2. rotate() -> rotiert sich um seine eigene Achse im Uhrzeigersinn
+    */
 function step() {
-    /*
+  /*
     FALLS OSTEN, DANN X UM EINS ERHÖHEN
     */
-    if (eddy.heading == "EAST") {
-        eddy.posX = eddy.posX + 1;
-    }
-    /*
-     FALLS SÜDEN, DANN Y UM EINS ERHÖREN
-         */
-    if(eddy.heading == "SOUTH"){
-        eddy.posY = eddy.posY + 1;
-    }
+  if (eddy.heading == "EAST") {
+    eddy.posX = eddy.posX + 1;
+  }
+  if (eddy.heading == "SOUTH") {
+    eddy.posX = eddy.posX + 1;
+  }
+  if (eddy.heading == "WEST") {
+    eddy.posX = eddy.posX - 1;
+  }
+  /*
+    FALLS SÜDEN, DANN Y UM EINS ERHÖREN
+    */
+  if (eddy.heading == "NORD") {
+    eddy.posY = eddy.posY - 1;
+  }
 
-    // FALLS WESTEN, DANN X UM EINS VERRINGERN
-    if(eddy.heading == "WEST") {
-        eddy.posX = eddy.posX - 1;
-    }
-    
-    //FALLS NORDEN, DANN Y UM EINS VERRINGERN
-    if(eddy.heading == "NORTH") {
-        eddy.posY = eddy.posY - 1;
-    }
+  /* dsjnksnkjsdnskjs
+    TODO: FALLS WESTEN, DANN X UM EINS VERRINGERN
+    TODO: FALLS NORDEN, DANN Y UM EINS VERRINGERN
+    */
 }
 
 function rotate() {
-    // FALLS OSTEN, DANN SÜDEN
-    if(eddy.heading == "EAST"){
-        eddy.heading = "SOUTH";
-    }
+  // FALLS OSTEN, DANN SÜDEN
+  if (eddy.heading == "OSTEN") {
+    eddy.heading = "SÜDEN";
+  }
 
-    // FALLS SÜDEN, DANN WESTEN
-    if(eddy.heading == "SOUTH"){
-        eddy.heading = "WEST";
-    }
+  if (eddy.heading == "SÜDEN") {
+    eddy.heading = "WEST";
+  }
 
-    // FALLS WESTEN, DANN NORDEN
-    if(eddy.heading == "WEST"){
-        eddy.heading = "NORTH";
-    }
+  if (eddy.heading == "WEST") {
+    eddy.heading = "NORDEN";
+  }
 
-    // FALLS NORDEN, DANN OSTEN
-    if(eddy.heading == "NORTH"){
-        eddy.heading = "EAST";
-    }
+  // FALLS SÜDEN, DANN WESTEN
+  if (eddy.heading == "NORD") {
+    eddy.heading = "EAST";
+  }
+  /*
+    TODO:FALLS WESTEN, DANN NORDEN
+    TODO:FALLS NORDEN, DANN Osten
+    */
 }
 
 /*
-CANVAS ZEICHNEN IN HTML UND ÜBER CONTEXT IMPORTIEREN
-ROBOTER EINZEICHNEN
-IN DER CANVAS BEWEGUNGEN AUSFÜHREN
-*/
+    CANVAS ZEICHNEN IN HTML UND ÜBER CONTEXT IMPORTIEREN
+    ROBOTER EINZEICHNEN
+    IN DER CANVAS BEWEGUNGEN AUSFÜHREN
+    */
