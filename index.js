1292c19bfc6886fed9f02b9dd312067713ce4fd0
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-12-14 10:28:31 +0100

    robot ü01

--- index.js
+++ index.js
@@ -31,10 +31,14 @@ function step() {
         eddy.posY = eddy.posY + 1;
     }
 
-    /* 
-     TODO: FALLS WESTEN, DANN X UM EINS VERRINGERN
-     TODO: FALLS NORDEN, DANN Y UM EINS VERRINGERN
-     */
+    // TODO: FALLS WESTEN, DANN X UM EINS VERRINGERN
+     if(eddy.heading == "WEST"){
+         eddy.posX + 1;
+     }
+    //TODO: FALLS NORDEN, DANN Y UM EINS VERRINGERN
+    if(eddy.heading == "North"){
+        eddy.posY + 1;
+    }
 }
 
 function rotate() {
@@ -47,10 +51,14 @@ function rotate() {
     if(eddy.heading == "SOUTH"){
         eddy.heading = "WEST";
     }
-    /*
-        TODO:FALLS WESTEN, DANN NORDEN
-        TODO:FALLS NORDEN, DANN Osten
-    */
+    //TODO:FALLS WESTEN, DANN NORDEN
+    if(eddy.heading == "WEST"){
+        eddy.heading = "NORTH";
+    }   
+    //TODO:FALLS NORDEN, DANN Osten
+    if(eddy.heading == "NORTH"){
+        eddy.heading = "EAST";
+    }
 }
 
 /*
